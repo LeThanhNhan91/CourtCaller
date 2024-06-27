337d79cec39541fc731206faa4a479b656c8cdcf
LeThanhNhan91 <lethanhnhan09012004@gmail.com>
2024-06-27 09:10:36 +0700

    edit payment confirmation

--- src/pages/user/Payment/PaymentFailed.js
+++ src/pages/user/Payment/PaymentFailed.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './FailurePage.css';
 import { Link } from "react-router-dom";
+import noImg from "assets/users/images/byday/red_cross.png"
 
 function PaymentFailed() {
   return (
     <div className="payment-container">
       <div className="success-box">
-        <img src="../../../assets/users/images/byday/red_cross.png" alt="Payment Failed" />
+        <img src={noImg} alt="Payment Failed" />
         <h2>OOPS !!!</h2>
         <p>Something went wrong! Please try again or contact <a href="mailto:courtcallers@gmail.com" className="contact-email">courtcallers@gmail.com</a></p>
         <button><Link to="/reject">Back to Booking Page</Link></button>
--- src/pages/user/Payment/PaymentSuccessful.js
+++ src/pages/user/Payment/PaymentSuccessful.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './SuccessPage.css';
 import { Link } from "react-router-dom";
+import yesImg from "assets/users/images/byday/green_tick.png"
 
 function PaymentSuccessful() {
   return (
     <div className="payment-container">
       <div className="success-box">
-        <img src="../../../assets/users/images/byday/green_tick.png" alt="Payment Successful" />
+        <img src={yesImg} alt="Payment Successful" />
         <h2>Thank You!</h2>
         <p>Your booking has been successfully submitted. Thanks for choosing our service</p>
-        <button><Link to="/">Back to Home</Link></button>
+        <button><Link to="/" style={{textDecoration: "none", color: "#fff"}}>Back to Home</Link></button>
       </div>
     </div>
   );
