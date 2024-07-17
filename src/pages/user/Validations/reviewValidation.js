22010639bef3344e48046d17fda3422e93276c3c
LeThanhNhan91 <lethanhnhan09012004@gmail.com>
2024-07-18 01:06:29 +0700

    Update reviewValidation.js

--- src/pages/user/Validations/reviewValidation.js
+++ src/pages/user/Validations/reviewValidation.js
@@ -1,5 +1,13 @@
-const API_URL = ""
+export const reviewTextValidation = (reviewText) => {
+    if(reviewText.length < 8){
+        return {isValid: false, message: 'You need to full fill this box. At least 8 characters!'}
+    }
+    return{isValid: true, message: ''}
+}
 
-export const requestValidation = (branchId) => {
-
+export const valueValidation = (value) => {
+    if(value < 1){
+        return {isValid: false, message: 'Choose number of stars !'}
+    }
+    return{isValid: true, message: ''}
 }
